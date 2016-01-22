50e470afb1753f7bcbe4a5351292125c05dafc4b
Martijn Verhoevengit config --global user.email florismartijnv@gmail.comcdmkdir ~/test-repogit config --global user.name Martijn <florismartijnv@gmail.com>
2016-01-22 15:57:04 -0500

    test

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a set of functions that could help create the inverse of a matrix,
+## especially if the matrix needs to be inversed repeatedly without the contents changing. 
+## The first function stores the matrix in the cache, with the second matrix loading the matrix
+## and creating its inverse. 
 
-## Write a short comment describing this function
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	i <<- NULL
+	set <- function(y) {
+		x <<- y
+		i <<- NULL
+	}
+	get <- function () x
+	setInverse <- function(inverse) i <<- inverse
+	getInverse <- function() i
+	list (set = set, get = get, setInverse = setInverse, getInverse = getInverse) 
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        i <- x$getInverse()
+        if(1is.null(i)) {
+        		message("Getting cached data")
+        		return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setInverse(i)
+        i
 }
